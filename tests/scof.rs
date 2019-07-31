0e52d579c2186bf257ab1637193dc7f84140dafb
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-07-31 18:41:10 -0500

    Move tests into 1 file.

---
+++ tests/scof.rs
@@ -0,0 +1,55 @@
+use muon_rs as muon;
+
+#[test]
+fn metadata() {
+    let input = include_str!("../scof/Meta.muon");
+
+    let meta: scof::Meta = muon::from_str(input).unwrap();
+
+    println!("{:?}", meta);
+
+    let output = muon::to_string(&meta).unwrap();
+
+    println!("{}", output);
+
+    let meta_clone: scof::Meta = muon::from_str(&output).unwrap();
+
+    assert_eq!(meta, meta_clone);
+    assert_eq!(input, output);
+}
+
+#[test]
+fn movement() {
+    let input = include_str!("../scof/Movement/The Beginning.muon");
+
+    let style: scof::Movement = muon::from_str(input).unwrap();
+
+    println!("{:?}", style);
+
+    let output = muon::to_string(&style).unwrap();
+
+    println!("{}", output);
+
+    let style_clone: scof::Movement = muon::from_str(&output).unwrap();
+
+    assert_eq!(style, style_clone);
+    assert_eq!(input, output);
+}
+
+#[test]
+fn style() {
+    let input = include_str!("../scof/Style.muon");
+
+    let style: scof::Style = muon::from_str(input).unwrap();
+
+    println!("{:?}", style);
+
+    let output = muon::to_string(&style).unwrap();
+
+    println!("{}", output);
+
+    let style_clone: scof::Style = muon::from_str(&output).unwrap();
+
+    assert_eq!(style, style_clone);
+    assert_eq!(input, output);
+}
